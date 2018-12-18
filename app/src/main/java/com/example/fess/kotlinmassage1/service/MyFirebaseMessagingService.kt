f8e8caaa1e4123e22064a3c52dba9014250d703f
FESS <dreamer.alex.toms@gmail.com>
2018-12-18 14:20:20 +0300

    Test-pushh-message-send-recieve
    Signed-off-by: FESS <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/fess/kotlinmassage1/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fess/kotlinmassage1/service/MyFirebaseMessagingService.kt
@@ -1,10 +1,12 @@
 package com.example.fess.kotlinmassage1.service
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.os.Build
 import android.support.v4.app.NotificationCompat
 import android.util.Log
 import android.widget.Toast
@@ -27,6 +29,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage!!.from)
         Log.d(TAG, "Notification Message Body: " + remoteMessage.notification!!.body!!)
+        // Check if message contains a data payload.
+        remoteMessage.data?.isNotEmpty()?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
         sendNotification(remoteMessage)
 //        val intent = Intent(this@MyFirebaseMessagingService, LoginActivity::class.java)
 //        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -95,14 +101,24 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT)
+        val channelId = getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
                 .setContentText(remoteMessage.notification!!.body)
+                .setContentTitle(getString(R.string.fcm_message))
                 .setAutoCancel(true)
                 .setSmallIcon(R.drawable.ic_fire_emoji)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 
